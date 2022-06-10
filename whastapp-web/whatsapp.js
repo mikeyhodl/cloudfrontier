0edbbc0d49c170461d05f67f4a63fd42706dd0f5
mikey <mike.owino40@yahoo.com>
2022-06-10 21:07:47 +0000

    ....

---
+++ whastapp-web/whatsapp.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+        (function () {
+            var options = {
+                whatsapp: "254115588872",
+                facebook: "246152853989129",
+                call_to_action: "",
+                position: "left",
+                button_color: "#54d6eb",
+                pre_filled_message: "Hello! Lancola Tech Company Limited.",
+                greeting_message: "Hello, how may we help you? Just send us a message now to get assistance.",
+            };
+            var proto = document.location.protocol, host = "thedentalspecialistscentre.co.ke", url = proto + "//cdn." + host;
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/init.js';
+            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+    </script>
